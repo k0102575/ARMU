49c8cb5b77632bc65bd93048684ba9deb603248c
WANZARGEN <wanjin911003@gmail.com>
2017-07-03 20:11:23 +0900

    join editing

--- src/main/webapp/join/index.js
+++ src/main/webapp/join/index.js
@@ -1,17 +1,17 @@
-window.$ = window.jQuery = require('jquery')
-
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
+var btn = $("#join-btn");
+console.log(btn);
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
+
+btn.click(function() {
+    console.log("aaa");
     modal.style.display = "block";
-}
+})
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -19,8 +19,8 @@ span.onclick = function() {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+//window.onclick = function(event) {
+//    if (event.target == modal) {
+//        modal.style.display = "none";
+//    }
+//}
