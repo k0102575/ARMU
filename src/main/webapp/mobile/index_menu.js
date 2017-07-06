a199eade90acb199243e88530654a2f8e5b1fc63
WANZARGEN <wanjin911003@gmail.com>
2017-07-06 15:00:37 +0900

    mobile menu

--- src/main/webapp/mobile/index_menu.js
+++ src/main/webapp/mobile/index_menu.js
@@ -1,15 +1,19 @@
 $(function(){
-  $('.nav').mobilemenu({
-    menuOpenIcon: '<span class="mobilemenu--open-icon mobilemenu-theme"><span class="slicknav_icon"><span class="slicknav_icon-bar slicknav_icon-bar--first"></span><span class="slicknav_icon-bar"></span><span class="slicknav_icon-bar"></span></span></span>',
-    onInit: function(menu, options){
-      menu.find('ul').removeClass('nav nav-pills pull-right');
-    }
-  });
-  
-var menu = $.mobilemenu({
-	  menuOpenIcon: $('.mobilemenu--open-icon'),
-	  body: '<p>메뉴메뉴메뉴메뉴</p>'
-	  
+	$('.nav').mobilemenu({
+		menuOpenIcon: '<span class="mobilemenu--open-icon mobilemenu-theme">\
+			<span class="slicknav_icon">\
+			<span class="slicknav_icon-bar slicknav_icon-bar--first"></span>\
+			<span class="slicknav_icon-bar"></span>\
+			<span class="slicknav_icon-bar"></span></span></span>',
+			onInit: function(menu, options){
+				menu.find('ul').removeClass('nav nav-pills pull-right');
+			}
+	});
+
+	var menu = $.mobilemenu({
+		menuOpenIcon: $('.mobilemenu--open-icon'),
+		body: '<p>메뉴메뉴메뉴메뉴</p>'
+
 	});
 });
 
