594e29e620735a8f55888ac3b3220c4f88fd37f6
WANZARGEN <wanjin911003@gmail.com>
2017-07-18 19:04:16 +0900

    추천 탭 제이슨 완료

--- src/main/webapp/mobile/main-recommend.js
+++ src/main/webapp/mobile/main-recommend.js
@@ -10,29 +10,14 @@ if(isEvent) {
   noEventView.css('display', 'none')
   yesEventView.css('display', '')
 
-  /*initialize swiper when document ready*/
-  $(document).ready(function () {
-    var mySwiper = new Swiper ('.swiper-container', {
-      pagination: '.swiper-pagination',
-      prevButton: '.swiper-button-prev',
-      nextButton: '.swiper-button-next',
-      centeredSlides: true,
-      paginationClickable: true,
-      spaceBetween: 30,
-      slidesPerView: 'auto',
-      cssWidthAndHeight: true,
-      paginationType: 'bullets',
-      paginationElement: 'span'
-    });
-  });//initialize swiper ended
-
-
+  displayRecommandByEventMusiList();
+  displayBestReviewMusiList();
+  displayPopularMusiList();
+  displayMostPopularCategoryList();
 } else {
   yesEventView.css('display', 'none')
   noEventView.css('display', '')
 }
-
-
 
 
 
@@ -41,6 +26,60 @@ function displayRecommandByEventMusiList() {
     var templateFn = Handlebars.compile($('#rec-by-event-musi-template').text())
     var generatedHTML = templateFn(result)
     var container = $('#rec-by-event-musi-container')
+    var html = container.html()
+    container.html(html + generatedHTML)
+
+    /*initialize swiper when document ready*/
+    $(document).ready(function () {
+      var mySwiper = new Swiper ('.swiper-container', {
+        pagination: '.swiper-pagination',
+        prevButton: '.swiper-button-prev',
+        nextButton: '.swiper-button-next',
+        centeredSlides: true,
+        paginationClickable: true,
+        spaceBetween: 30,
+        slidesPerView: 'auto',
+        cssWidthAndHeight: true,
+        paginationType: 'bullets',
+        paginationElement: 'span'
+      });
+    });//initialize swiper ended
+  }, function(err) {
+    console.log(err)
+  })
+}
+
+
+function displayBestReviewMusiList() {
+  $.getJSON('json/rec-best-review-musi.json', function(result) {
+    var templateFn = Handlebars.compile($('#rec-best-review-musi-template').text())
+    var generatedHTML = templateFn(result)
+    var container = $('#rec-best-review-musi-container')
+    var html = container.html()
+    container.html(html + generatedHTML)
+
+  }, function(err) {
+    console.log(err)
+  })
+}
+
+function displayPopularMusiList() {
+  $.getJSON('json/rec-popular-musi.json', function(result) {
+    var templateFn = Handlebars.compile($('#rec-popular-musi-template').text())
+    var generatedHTML = templateFn(result)
+    var container = $('#rec-popular-musi-container')
+    var html = container.html()
+    container.html(html + generatedHTML)
+  }, function(err) {
+    console.log(err)
+  })
+}
+
+function displayMostPopularCategoryList() {
+  $.getJSON('json/rec-most-popular-category.json', function(result) {
+    var templateFn = Handlebars.compile($('#rec-most-popular-category-template').text())
+    var generatedHTML = templateFn(result)
+    var container = $('#rec-most-popular-category-container')
     var html = container.html()
     container.html(html + generatedHTML)
   }, function(err) {
