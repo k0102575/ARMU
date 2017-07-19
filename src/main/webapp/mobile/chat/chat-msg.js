943ea772bfee8a6deeafa0748c5e23c530eb867a
WANZARGEN <wanjin911003@gmail.com>
2017-07-19 20:45:18 +0900

    .

--- src/main/webapp/mobile/chat/chat-msg.js
+++ src/main/webapp/mobile/chat/chat-msg.js
@@ -1,6 +1,5 @@
 "use strict"
 
-
 var myAlias = true,
   value = "abc",
   messageBox = $('#messageBox'),
@@ -19,29 +18,30 @@ sendBtn.on('click', function() {
   }
 })
 
-
 $(document).ready(function() {
-      $(".animsition").animsition({
-        inClass: 'fade-in-up',
-        outClass: 'fade-out-down',
-        inDuration: 500,
-        outDuration: 700,
-        linkElement: '.animsition-link',
-        // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-        loading: true,
-        loadingParentElement: 'body', //animsition wrapper element
-        loadingClass: 'animsition-loading',
-        loadingInner: '', // e.g '<img src="loading.svg" />'
-        timeout: false,
-        timeoutCountdown: 5000,
-        onLoadEvent: true,
-        browser: ['animation-duration', '-webkit-animation-duration'],
-        // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-        // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-        overlay: false,
-        overlayClass: 'animsition-overlay-slide',
-        overlayParentElement: 'body',
-        transition: function(url) {
-          window.location.href = url;
-        }
-      });
+  $(".animsition").animsition({
+    inClass: 'fade-in-up',
+    outClass: 'fade-out-down',
+    inDuration: 500,
+    outDuration: 700,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+    loading: true,
+    loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: ['animation-duration', '-webkit-animation-duration'],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay: false,
+    overlayClass: 'animsition-overlay-slide',
+    overlayParentElement: 'body',
+    transition: function(url) {
+      window.location.href = url;
+    }
+});
+
+})
