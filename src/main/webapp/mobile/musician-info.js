bfc68b83bdbc12d49fc0661a70c9a0b096ff520a
SeungMin Kim <k0102027@naver.com>
2017-07-21 20:02:06 +0900

    뮤지션 상세정보 타임라인
    뮤지션 상세정보 타임라인

--- src/main/webapp/mobile/musician-info.js
+++ src/main/webapp/mobile/musician-info.js
@@ -1,14 +1,26 @@
 var lastScroll = 0,
     musicianHeaderInfoImg = $("#musician-header-info-img"),
-    moveTopBtn = $("#move_top_btn")
-
+    moveTopBtn = $("#move_top_btn"),
+    musicianInfoBtnContainer = $("#musician-info-btn-container"),
+    musicianHeaderInfoImg = $("#musician-header-info-img")
     
+    console.log($("div[name=timeline-container]").length)
     
     $(window).scroll(function(event){
+    
+    if($(document).scrollTop() > 545) {
+      musicianInfoBtnContainer.css("position", "fixed")
+      musicianHeaderInfoImg.css("top", "9%")
+    } else if ($(document).scrollTop() < 545) {
+      musicianInfoBtnContainer.css("position", "relative")
+      musicianHeaderInfoImg.css("top", "18%")
+    }
+    
     var st = $(this).scrollTop();
     if (st > lastScroll){
       moveTopBtn.css("display", "block")
       musicianHeaderInfoImg.css("display", "block")
+
     } else {
       moveTopBtn.css("display", "none")
       musicianHeaderInfoImg.css("display", "none")
