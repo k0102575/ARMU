0bb0af19f3e907d797ab21abcd30fe411b05ed9c
been47 <gh954023@gmail.com>
2017-07-24 20:54:39 +0900

    '필터고침'

--- src/main/webapp/js/mobile/gmode/main-surf.js
+++ src/main/webapp/js/mobile/gmode/main-surf.js
@@ -30,31 +30,75 @@ filterCancel.on('click', function() {
 })
 
 var xScroll = new jindo.m.Scroll("filter-loc-tab", {
-  bUseHScroll: true,
-  bUseVScroll: false,
-  bUseMomentum: false,
-  nHeight: 100,
-  nWidth: 980
+    bUseHScroll: true,
+    bUseVScroll: false,
+    bUseMomentum: false,
+    nHeight: 100,
+    nWidth: 980
 });
 
 var yScroll = new jindo.m.Scroll("seoul", {
-  bUseHScroll: false,
-  bUseVScroll: true,
-  bUseMomentum: false,
-  nHeight: 340
+    bUseHScroll: false,
+    bUseVScroll: true,
+    bUseMomentum: false,
+    nHeight: 400
 });
-
+yScroll = new jindo.m.Scroll("kyeonggi", {
+    bUseHScroll: false,
+    bUseVScroll: true,
+    bUseMomentum: false,
+    nHeight: 400
+});
+yScroll = new jindo.m.Scroll("local", {
+    bUseHScroll: false,
+    bUseVScroll: true,
+    bUseMomentum: false,
+    nHeight: 400
+});
+yScroll = new jindo.m.Scroll("incheon", {
+    bUseHScroll: false,
+    bUseVScroll: true,
+    bUseMomentum: false,
+    nHeight: 400
+});
+yScroll = new jindo.m.Scroll("busan", {
+    bUseHScroll: false,
+    bUseVScroll: true,
+    bUseMomentum: false,
+    nHeight: 400
+});
+yScroll = new jindo.m.Scroll("daegu", {
+    bUseHScroll: false,
+    bUseVScroll: true,
+    bUseMomentum: false,
+    nHeight: 400
+});
+yScroll = new jindo.m.Scroll("gwangju", {
+    bUseHScroll: false,
+    bUseVScroll: true,
+    bUseMomentum: false,
+    nHeight: 400
+});
+yScroll = new jindo.m.Scroll("daejun", {
+    bUseHScroll: false,
+    bUseVScroll: true,
+    bUseMomentum: false,
+    nHeight: 400
+});
 $('#filter-loc').click(function() {
-  $("#filter-loc-backscreen").css('display', 'block');
-  $("#filter-loc-toggle").css('display', 'none');
-  $("#filter-loc-toggle").css('bottom', '0%');
-  $("#filter-loc-toggle").toggle("slide", {direction: "down"});
+    $("#filter-loc-backscreen").css('display', 'block');
+    $('#musician-surf-list').css('overflow','hidden').css('position','fixed')
+    $("#filter-loc-toggle").css('visibility', 'visible');
+    $(".filter-loc-sub-con").css('visibility','visible');
+    // $("#filter-loc-toggle").toggle("slide", {direction: "down"});
 });
 
 $("#filter-loc-backscreen").click(function() {
-  $("#filter-loc-toggle").toggle("slide", {direction: "down"});
-  $("#filter-loc-toggle").css('bottom', '-50%');
-  $("#filter-loc-backscreen").css('display', 'none');
+    // $("#filter-loc-toggle").toggle("slide", {direction: "down"});
+    $('#musician-surf-list').css('overflow','').css('position','absolute')
+    $("#filter-loc-toggle").css('visibility', 'hidden');
+    $(".filter-loc-sub-con").css('visibility','hidden')
+    $("#filter-loc-backscreen").css('display', 'none');
 })
 
 $( function() {
@@ -111,4 +155,28 @@ seoul.click(function () {
     seoul.removeClass('on2')
     $(this).html('<i class="fa fa-check" aria-hidden="true"></i>'+ $(this).text())
     $(this).addClass('on2')
+})
+
+
+$(function ($) {
+    let index =null;
+    // let $filterwrap = $('.filter-loc-toggle')
+    let $fcon1 = $('.filter-loc-sub-tab')
+    let $fcon2 = $('.filter-loc-sub-con')
+
+    console.log($fcon2)
+
+    tabMenu($fcon1, $fcon2)
+    function tabMenu(els, con) {
+      els.on('click', function () {
+          els.removeClass('on')
+          $(this).addClass('on')
+          index = $(this).index()
+
+          con.css("visibility","hidden")
+          con.css("height","0")
+          con.eq(index).css("visibility","visible")
+          con.eq(index).css("height","400px")
+      })
+    }
 })
