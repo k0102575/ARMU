352a167258de1d7c1b8d94caf8dcc6a2c620fa1c
SeungMin Kim <k0102027@naver.com>
2017-07-25 12:32:55 +0900

    시작파일 설정 이벤트등록하기 수정
    시작파일 설정 이벤트등록하기 수정

--- src/main/webapp/js/mobile/gmode/event-regist/index.js
+++ src/main/webapp/js/mobile/gmode/event-regist/index.js
@@ -4,46 +4,89 @@ var progressBar = $("#event-progressbar"),
     pageCancelPageBackscreen = $("#event-cancelpage-backscreen"),
     pageCancelPageReturn = $("#event-cancelpage-return"),
     pageCancelPageQuit = $("#event-cancelpage-quit"),
+    
+    // 이벤트 프로그레스바 밑 취소페이지
+    
     eventPage1 = $("#event-page1"),
     eventPage1Cancel = $("#event-page1-cancel"),
     eventPage1Next = $("#event-page1-next"),
+    
+    //  이벤트 1페이지
+    
     eventPage2 = $("#event-page2"),
     eventPage2Prev = $("#event-page2-prev"),
     eventPage2Next = $("#event-page2-next"),
+    themeSelectMenu = $("#theme-select-menu"),
+    majorSelectMenu = $("#major-select-menu"),
+    genereSelectMenu = $("#genre-select-menu"),
+    themeSelectVal = 0,
+    majorSelectVal = 0,
+    genreSelectVal = 0,
+    
+    //  이벤트 2페이지
+    
     eventPage3 = $("#event-page3"),
     eventPage3Prev = $("#event-page3-prev"),
     eventPage3Next = $("#event-page3-next"),
     eventPage3Calendar = $("#calendar-page3"),
+    eventDate = 0,
+    
+    // 이벤트 3페이지
+    
     eventPage4 = $("#event-page4"),
     eventPage4Prev = $("#event-page4-prev"),
     eventPage4Next = $("#event-page4-next"),
+    sidoSelectMenu =  $("#sido-select-menu"),
+    citySelectMenu =  $("#city-select-menu"),
+    streetSelectMenu =  $("#street-select-menu"),
+    DetailLocation = $("#detail-location"),
+    sidoSelectVal = 0,
+    citySelectVal = 0,
+    streetSelectVal = 0,
+    DetailLocationVal = 0,
+    
+    // 이벤트 4페이지
+    
+    
     eventPage5 = $("#event-page5"),
     eventPage5Prev = $("#event-page5-prev"),
-    eventPage5Next = $("#event-page5-next"),
+    eventPage5Next = $("#event-page5-next"), 
+    inputEventName =  $("#event-name"),
+    inputEventPay =  $("#event-pay"),
+    inputEventRequire =  $("#event-require"),
+    EventNameVal = 0, 
+    EventPayVal = 0, 
+    EventRequireVal = 0,
+    
+    // 이벤트 5페이지
+    
     eventPage6 = $("#event-page6"),
     eventPage6Prev = $("#event-page6-prev"),
     eventPage6Next = $("#event-page6-next"),
+    eventRehearseText = $("#event-rehearse-text"),
     inputRehearseText = $("#input-rehearse-text"),
     inputRehearseCount = $("#input-rehearse-count"),
     inputRehearsePay = $("#input-rehearse-pay"),
+    
+    // 이벤트 6 페이지
+    
     eventPage7 = $("#event-page7"),
     eventPage7Prev = $("#event-page7-prev"),
     eventPage7Next = $("#event-page7-next"),
-    eventPage8 = $("#event-page8"),
-    eventPage8Home = $("#event-page8-home"),
     nameConfirm = $("#name-confirm"),
-    cathgoryConfirm = $("#name-confirm"),
-    dateConfirm = $("#name-confirm"),
-    matchConfirm = $("#name-confirm"),
-    locationConfirm = $("#name-confirm"),
-    payConfirm = $("#name-confirm"),
-    requireConfirm = $("#name-confirm")
+    cathgoryConfirm = $("#cathgory-confirm"),
+    dateConfirm = $("#date-confirm"),
+    locationConfirm = $("#location-confirm"),
+    payConfirm = $("#pay-confirm"),
+    requireConfirm = $("#require-confirm"),
     
-var themeVal,majorVal,genreVal,eventDate,
-    sido,city,street,detailLocation,
-    eventName, eventPay, eventRequire,
-    matchDate
-
+    // 이벤트 7 페이지
+    
+    eventPage8 = $("#event-page8"),
+    eventPage8Home = $("#event-page8-home")
+    
+    // 이벤트 8페이지 
+    
 $(document).ready(function() {
   eventPage1.toggle();
   progress(5)
@@ -81,9 +124,9 @@ eventPage2Prev.on('click', function() {
 eventPage2Next.on('click', function() {
   eventPage2.toggle(0);
   eventPage3.toggle(0 , function() {
-    themeVal =  $("#theme-select-menu").val()
-    majorVal =  $("#major-select-menu").val()
-    genreVal =  $("#genre-select-menu").val()
+    themeSelectVal = themeSelectMenu.val()
+    majorSelectVal = majorSelectMenu.val()
+    genreSelectVal = genereSelectMenu.val()
     progress(35)
     eventPage3Calendar.datepicker({
       dayNames: ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],
@@ -102,7 +145,7 @@ eventPage3Prev.on('click', function() {
 eventPage3Next.on('click', function() {
   eventPage3.toggle(0);
   eventPage4.toggle(0 , function() {
-    eventDate = $("#calendar-page3").val()
+    eventDate = eventPage3Calendar.val()
     progress(50)
   });
 })
@@ -110,17 +153,17 @@ eventPage3Next.on('click', function() {
 eventPage4Prev.on('click', function() {
   eventPage4.toggle(0);
   eventPage3.toggle(0 , function() {
-    progress(50)
+    progress(35)
   });
 })
 
 eventPage4Next.on('click', function() {
   eventPage4.toggle(0);
   eventPage5.toggle(0 , function() {
-    sido =  $("#sido-select-menu").val()
-    city =  $("#city-select-menu").val()
-    street =  $("#street-select-menu").val()
-    detailLocation = $("#detail-location").val()
+    sidoSelectVal = sidoSelectMenu.val()
+    citySelectVal = citySelectMenu.val()
+    streetSelectVal = streetSelectMenu.val()
+    DetailLocationVal = DetailLocation.val()
     progress(65)
   });
 })
@@ -135,9 +178,10 @@ eventPage5Prev.on('click', function() {
 eventPage5Next.on('click', function() {
   eventPage5.toggle(0);
   eventPage6.toggle(0 , function() {
-    eventName =  $("#event-name").val()
-    eventPay =  $("#event-pay").val()
-    eventRequire =  $("#event-require").val()
+    
+    EventNameVal = inputEventName.val(), 
+    EventPayVal = inputEventPay.val(), 
+    EventRequireVal = inputEventRequire.val(),
     progress(80)
   });
 })
@@ -158,13 +202,15 @@ eventPage6Next.on('click', function() {
 
 $(".btn").on('click', function(event) {
   if(event.currentTarget.innerText == "네") {
+    eventRehearseText.css("display", "block")
 	  inputRehearseText.css("display", "block")
-  	  inputRehearseCount.css("display", "block")
-  	  inputRehearsePay.css("display", "block")
+	  inputRehearseCount.css("display", "block")
+	  inputRehearsePay.css("display", "block")
   } else if (event.currentTarget.innerText == "아니요") {
+    eventRehearseText.css("display", "none")
 	  inputRehearseText.css("display", "none")
-  	  inputRehearseCount.css("display", "none")
-  	  inputRehearsePay.css("display", "none")
+	  inputRehearseCount.css("display", "none")
+	  inputRehearsePay.css("display", "none")
   }
   
 })
@@ -193,3 +239,20 @@ function progress(per) {
     create: function(event, ui) {$(this).find('.ui-widget-header').css({'background-color':'#525252'})}
   });
 };
+
+sidoSelectVal = 0,
+citySelectVal = 0,
+streetSelectVal = 0,
+DetailLocationVal = 0,
+themeSelectVal = 0,
+majorSelectVal = 0,
+genreSelectVal = 0,
+
+function eventConfirm() {
+  nameConfirm = EventNameVal
+  cathgoryConfirm
+  dateConfirm = eventDate
+  locationConfirm
+  payConfirm = EventPayVal
+  requireConfirm = EventRequireVal
+}
