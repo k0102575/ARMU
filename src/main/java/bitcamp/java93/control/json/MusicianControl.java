a5d975cc8b32a2fd5f93bc56cb78e12236bc2512
been47 <gh954023@gmail.com>
2017-08-11 10:12:17 +0900

    .
    .

--- src/main/java/bitcamp/java93/control/json/MusicianControl.java
+++ src/main/java/bitcamp/java93/control/json/MusicianControl.java
@@ -319,7 +319,6 @@ public class MusicianControl {
     HashMap<String,Object> dataMap = new HashMap<>();
     List<Musician> search= (List<Musician>)musicianService.searchMusician(getLoginMember(session).getNo() ,location,major,genre,indexL,indexM,indexG,gender, minAge, maxAge);
     dataMap.put("listSurf", search);
-    System.out.println(genre);
     return new JsonResult(JsonResult.SUCCESS, dataMap);
   }
   
