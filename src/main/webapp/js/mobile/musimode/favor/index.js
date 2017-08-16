82a3d33b3c47b653efa5e0993f398f3d25e0f153
SeungMin Kim <k0102575@nate.com>
2017-08-16 17:39:09 +0900

    기트
    기트

--- src/main/webapp/js/mobile/musimode/favor/index.js
+++ src/main/webapp/js/mobile/musimode/favor/index.js
@@ -6,7 +6,6 @@ HandlebarsIntl.registerWith(Handlebars);
     
 function  displayFavorEventList(){
 $.getJSON('/event/listFavor.json', function(result) {
-  console.log(result)
   var templateFn = Handlebars.compile($('#event-favor-template').text())
   var generatedHTML = templateFn(result.data)
   var container = $('#event-favor-list')
@@ -42,4 +41,39 @@ moveTopBtn.on('click', function() {
 
 eventFavorPrev.on('click', function() {
   location.href = "/mobile/musimode/index.html"
+})
+
+$('body').on('click', ".event-favor-delete-button", function() {
+  $.post('/event/favorRemove.json', {
+    'no': $(this).val()
+  }, function(result) {
+	  console.log(result)
+    if(result.status == "success") {
+      swal({
+        title: "삭제에 성공했습니다",
+        type: "success",
+        showCancelButton: false,
+        confirmButtonColor: "#8069ef",
+        confirmButtonText: "확인",
+        customClass: "checkSwal"
+      }, function() {
+        location.reload()
+      });
+    }
+  }, 'json')
+})
+
+$('body').on('click', ".event-favor-writerbox", function() {
+	console.log($(this).parent().attr('data-no'))
+	  /*location.href = '/mobile/musimode/event-info/index.html?no=' + */
+})
+
+$('body').on('click', ".event-favor-title", function() {
+	console.log($(this).parent().attr('data-no'))
+	  /*location.href = '/mobile/musimode/event-info/index.html?no=' + */
+})
+
+$('body').on('click', ".event-favor-dscpbox", function() {
+	console.log($(this).parent().attr('data-no'))
+	  /*location.href = '/mobile/musimode/event-info/index.html?no=' + */
 })
