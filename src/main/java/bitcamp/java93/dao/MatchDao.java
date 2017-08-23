665438c1851cd33a0e591ac8240f31c0d4fcbc3e
SeungMin Kim <k0102027@naver.com>
2017-08-23 17:19:50 +0900

    notification 메서드 등록
    notification 메서드 등록

--- src/main/java/bitcamp/java93/dao/MatchDao.java
+++ src/main/java/bitcamp/java93/dao/MatchDao.java
@@ -7,6 +7,15 @@ import bitcamp.java93.domain.Event;
 import bitcamp.java93.domain.Musician;
 
 public interface MatchDao {
+  int selectExistPrCount(HashMap<String, Object> valueMap);//기존에 홍보한 데이터가 있는지 확인
+  int selectExistAppyCount(HashMap<String, Object> valueMap);//기존에 지원한 데이터가 있는지 확인
+  int checkPrActive(int prno);//홍보를 취소했는지 확인
+  int checkAppyActive(int appyno);//지원을 취소했는지 확인
+  int checkPrStatus(int prno);//홍보를 거절했는지 확인
+  int checkAppyStatus(int appyno);//지원을 거절했는지 확인
+  
+  
+  
   void prEvent(Map<String,Object> valueMap); // 일반모드 > 뮤지션 상세 페이지 > 뮤지션 홍보
   void prUpdate(HashMap<String, Object> valueMap); // 일반모드 > 뮤지션 상세 페이지 > 뮤지션 홍보
   void appyEvent(Map<String,Object> valueMap); // 뮤지션 모드 > 이벤트 상세페이지 > 뮤지션 지원 추가
@@ -20,10 +29,4 @@ public interface MatchDao {
   void deletePr(HashMap<String,Object> valueMap);//일반모드 > 나의 이벤트 > 모집중 > 내가 요청한 뮤지션 > 요청 취소
   int selectAppyCount(HashMap<String, Object> valueMap); /* 뮤지션 모드 > 이벤트 상세페이지 > 지원 활성여부 확인 */
   void updateReview(Event event); // 리뷰 추가
-  int selectExistPrCount(HashMap<String, Object> valueMap);//기존에 홍보한 데이터가 있는지 확인
-  int selectExistAppyCount(HashMap<String, Object> valueMap);//기존에 지원한 데이터가 있는지 확인
-  int checkPrActive(int prno);//홍보를 취소했는지 확인
-  int checkAppyActive(int appyno);//지원을 취소했는지 확인
-  int checkPrStatus(int prno);//홍보를 거절했는지 확인
-  int checkAppyStatus(int appyno);//지원을 거절했는지 확인
 }
--- src/main/java/bitcamp/java93/dao/NotificationDao.java
+++ src/main/java/bitcamp/java93/dao/NotificationDao.java
@@ -1,6 +1,5 @@
 package bitcamp.java93.dao;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,10 +8,17 @@ import bitcamp.java93.domain.Notification;
 public interface NotificationDao {
   List<Notification> selectNotiList(int no);
   List<Notification> selectMusiNotiList(int no);
-  int insertEventPrNoti(Map<String,Object> valueMap); // 뮤지션에게 일반인이 pr
-  int insertEventAppyNoti(Map<String,Object> valueMap); // 일반인에게 뮤지션이 appy
-  int deleteEventAppyNoti(Map<String,Object> valueMap); // 뮤지션이 appy 취소시 있던 noti 삭제
-  void insertEventEditNoti(HashMap<String, Object> valueMap); // 뮤지션에게 이벤트 편집
-  void insertEventDeleteNoti(HashMap<String, Object> valueMap); // 뮤지션에게 이벤트 삭제
-  void insertEventRevNoti(HashMap<String, Object> valueMap);  // 뮤지션에게 리뷰 작성
+  void insertEventPrNoti(Map<String,Object> valueMap); // 1.뮤지션에게 일반인이 pr
+  void rejectEventPrNoti(Map<String,Object> valueMap); // 2.뮤지션이 pr 거절
+  void acceptEventPrNoti(Map<String,Object> valueMap); // 3. 뮤지션이 pr 수락
+  void insertEventAppyNoti(Map<String,Object> valueMap); // 4.일반인에게 뮤지션이 appy
+  void rejectEventAppyNoti(Map<String,Object> valueMap); // 5.일반인이 appy 거절
+  void insertEventMtcNoti(Map<String,Object> valueMap); // 6.일반인이 매칭 확정
+  void insertEventExpiredNoti(Map<String,Object> valueMap); // 7.미확정 이벤트 당일 지남
+  void insertEventTodayNoti(Map<String,Object> valueMap); // 8.확정 이벤트 당일
+  void insertEventEditNoti(Map<String,Object> valueMap); // 9.뮤지션에게 이벤트 편집
+  void insertEventDeleteNoti(Map<String,Object> valueMap); // 10.뮤지션에게 이벤트 삭제
+  void deleteEventPrNoti(int prno); // 11.일반인이 pr 취소
+  void deleteEventAppyNoti(int appyno); // 12.뮤지션이 appy 취소시 있던 noti 삭제
+  void insertEventRevNoti(Map<String,Object> valueMap); // 13.뮤지션에게 리뷰 입력
 }
